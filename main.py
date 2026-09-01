250c1780fc5c814b973644f81d0201a328741f67
Pablo Barroso González <pablo.barroso@proton.me>
2026-09-01 15:30:29 +0200

    calculando y mostarando por consola la media general de wpm

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import pandas as pd
 
 df = pd.read_csv("data/results.csv")
 
-primeras_filas = df.head()
-ultimas_filas = df.tail()
-filas, columas = df.shape
+wpm_media =df["wpm"].mean()
+print(f"La wpm media general es {wpm_media:.2f}.")
 
-print(f"El df tiene:\nFilas:{filas}\nColumnas:{columas}")
-print(f"Las primeras filas del df son:\n{primeras_filas} y las ultimas son:\n{ultimas_filas}")
+
+
+#primeras_filas = df.head()
+#ultimas_filas = df.tail()
+#nombres_columnas = df.columns
+#
+#print(f"El df tiene:\nFilas:{filas}\nColumnas:{columnas}")
+#print(f"Las primeras filas del df son:\n{primeras_filas} y las ultimas son:\n{ultimas_filas}")
+#print("-"*30)
+#print(nombres_columnas)#
